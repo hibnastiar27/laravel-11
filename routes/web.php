7c19568a496b3059b3018a7f7780bb47556b7961
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-04 00:14:23 +0700

    remove commented-out code and clean up routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,3 @@ Route::get('/about', function () {
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 Route::get('/blog/{id}', [BlogController::class, 'show'])->name('blog.show');
 Route::get('/kontak', [KontakController::class, 'index'])->name('kontak');
-
-
-//  -> ->>>>>
-//  =/
-
-
-// 2 rute baru
-// 1. /blog [2 buah artikel, judul & isi]
-// 2. /contact [email sosmed / apapun]
