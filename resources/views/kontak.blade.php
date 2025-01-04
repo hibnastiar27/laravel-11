83781cb90d1bb511e067da9fdd936a282299d1f2
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-04 09:49:29 +0700

    refactor: responsive view kontak

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 {{-- CONTENT --}}
 <main class="h-screen">
-  <div class="grid grid-cols-3 gap-4 px-20 py-6">
-    <div class="col-span-1">
+  <div class="grid md:grid-cols-3 gap-4 p-6 md:px-20">
+    <div class="md:col-span-1">
       <h1 class="text-4xl font-bold">Connect With Us</h1>
       <div class="sosmed flex gap-4 mt-4">
         <a href="https://www.instagram.com/aria.justtry/" target="_blank">
@@ -13,12 +13,12 @@
         <a href="https://www.facebook.com/nurhibnastiar/" target="_blank">
           <img src="img/facebook.png" class="w-12" alt="facebook">
         </a>
-        <a href="https://www.x.com/@Hypenastiar/" target="_blank">
+        <a href="https://www.x.com/arialog1/" target="_blank">
           <img src="img/twitter.png" class="w-12" alt="x">
         </a>
       </div>
     </div>
-    <form class="col-span-2 flex flex-col gap-4">
+    <form class="md:col-span-2 flex flex-col gap-4">
       <label class="font-medium" for="email">Email</label>
       <input type="text" class="border rounded p-2 focus:outline-red-500" placeholder="Masukan Email" id="email">
 
