ba4cf938a70fdfe3830eeb2e209d1ef1c5109123
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-05 18:34:13 +0700

    refactor: enhance kontak page layout and input styles

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.guest')
 
 @section('content')
-{{-- CONTENT --}}
-<main class="h-screen">
-  <div class="grid md:grid-cols-3 gap-4 p-6 md:px-20">
+  {{-- CONTENT --}}
+  <div class="grid gap-4 p-6 md:m-auto md:max-w-screen-lg md:grid-cols-3">
     <div class="md:col-span-1">
       <h1 class="text-4xl font-bold">Connect With Us</h1>
-      <div class="sosmed flex gap-4 mt-4">
+      <div class="sosmed mt-4 flex gap-4">
         <a href="https://www.instagram.com/aria.justtry/" target="_blank">
           <img src="img/instagram.png" class="w-12" alt="instagram">
         </a>
@@ -18,16 +17,20 @@
         </a>
       </div>
     </div>
-    <form class="md:col-span-2 flex flex-col gap-4">
-      <label class="font-medium" for="email">Email</label>
-      <input type="text" class="border rounded p-2 focus:outline-red-500" placeholder="Masukan Email" id="email">
+    <form class="flex flex-col gap-4 md:col-span-2">
+      <div class="flex flex-col gap-2">
+        <label class="font-medium" for="email">Email</label>
+        <input type="text" class="rounded-lg border p-2 focus:outline-blue-500" placeholder="Masukan Email"
+          id="email">
+      </div>
 
-      <label class="font-medium" for="pesan">Pesan</label>
-      <textarea class="border rounded p-2 focus:outline-red-500" placeholder="Ketik Pesan" name="pesan" id="pesan"
-        cols="30" rows="10"></textarea>
+      <div class="flex flex-col gap-2">
+        <label class="font-medium" for="pesan">Pesan</label>
+        <textarea class="rounded-lg border p-2 focus:outline-blue-500" placeholder="Ketik Pesan" name="pesan" id="pesan"
+          cols="30" rows="10"></textarea>
+      </div>
 
-      <button class="border rounded-md px-4 py-2 bg-red-500 text-white">Kirim</button>
+      <button class="rounded-lg border bg-gradient-to-r from-blue-900 to-blue-500 p-3 text-white">Kirim</button>
     </form>
   </div>
-</main>
 @endsection
