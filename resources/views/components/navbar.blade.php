761b8a72ab93bd827920ff7a071b3acae3f0340c
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-05 16:18:48 +0700

    refactor: update navbar styles and improve link color

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,13 @@
-<nav class="flex w-full justify-between p-4 md:px-20 bg-slate-100">
-  <h1 class="text-4xl font-bold italic text-red-500">
+<nav class="flex w-full justify-between bg-slate-100 p-4 md:px-20">
+  <h1 class="bg-gradient-to-r from-blue-900 to-blue-500 bg-clip-text pb-1 text-4xl font-bold italic text-transparent">
     <a href="{{ url('/blog') }}">
       AriaLog
     </a>
   </h1>
   <div class="flex gap-4 *:p-2">
-    <a href="{{ route('blog') }}" class="{{ request()->routeIs('blog') ? 'text-red-500 font-bold' : '' }}">Blog</a>
-    <a href="{{ route('kelas') }}" class="{{ request()->routeIs('kelas') ? 'text-red-500 font-bold' : '' }}">Kelas</a>
-    <a href="{{ route('kontak') }}" class="{{ request()->routeIs('kontak') ? 'text-red-500 font-bold' : '' }}">Contact</a>
+    <a href="{{ route('blog') }}" class="{{ request()->routeIs('blog') ? 'text-blue-500 font-bold' : '' }}">Blog</a>
+    <a href="{{ route('kelas') }}" class="{{ request()->routeIs('kelas') ? 'text-blue-500 font-bold' : '' }}">Kelas</a>
+    <a href="{{ route('kontak') }}"
+      class="{{ request()->routeIs('kontak') ? 'text-blue-500 font-bold' : '' }}">Contact</a>
   </div>
 </nav>
