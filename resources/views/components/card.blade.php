f429b8debcb190bb9be9e823341f1cee08e40ddc
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-06 14:55:06 +0700

    feat: enhance blog layout with welcome section and latest posts display

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,7 +2,7 @@
   class="card flex flex-col items-center gap-4 rounded-xl border-2 border-slate-100 border-transparent bg-white p-4 shadow-lg duration-300 hover:border-blue-500 md:flex-row">
   <img src="{{ $gambar }}" class="h-40 w-full rounded-xl object-cover md:h-40 md:w-40" alt="html">
   <div class="card-body flex flex-col gap-3">
-    <h1 class="text-4xl font-bold">{{ $judul }}</h1>
-    <p>{{ $slot }}</p>
+    <h1 class="font-serif text-3xl font-bold">{{ $judul }}</h1>
+    <p class="line-clamp-3">{{ $slot }}</p>
   </div>
 </a>
