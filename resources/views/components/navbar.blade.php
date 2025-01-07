1cb665884d217409b3ed9d6cb8055029b37951cb
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-07 20:41:29 +0700

    refactor: improve navbar layout and styling for better responsiveness

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,15 @@
-<nav class="flex w-full justify-between bg-slate-100 p-4 md:px-20">
-  <h1 class="bg-gradient-to-r from-blue-900 to-blue-500 bg-clip-text pb-1 text-4xl font-bold italic text-transparent">
-    <a href="{{ url('/blog') }}">
-      AriaLog
-    </a>
-  </h1>
-  <div class="flex gap-4 *:p-2">
-    <a href="{{ route('blog') }}" class="{{ request()->routeIs('blog') ? 'text-blue-500 font-bold' : '' }}">Blog</a>
-    <a href="{{ route('kelas') }}" class="{{ request()->routeIs('kelas') ? 'text-blue-500 font-bold' : '' }}">Kelas</a>
-    <a href="{{ route('kontak') }}"
-      class="{{ request()->routeIs('kontak') ? 'text-blue-500 font-bold' : '' }}">Contact</a>
-  </div>
-</nav>
+<div class="md:mx-auto md:mt-4 md:max-w-screen-xl md:px-4">
+  <nav class="flex w-full items-center gap-4 rounded-lg bg-slate-100 p-4 px-6">
+    <h1 class="bg-gradient-to-r from-blue-900 to-blue-500 bg-clip-text pb-1 text-4xl font-bold italic text-transparent">
+      <a href="{{ url('/blog') }}">
+        AriaLog
+      </a>
+    </h1>
+    <div class="flex gap-4 *:p-2">
+      <a href="{{ route('blog') }}" class="{{ request()->routeIs('blog') ? 'text-blue-500 font-bold' : '' }}">Blog</a>
+      <a href="{{ route('kelas') }}" class="{{ request()->routeIs('kelas') ? 'text-blue-500 font-bold' : '' }}">Kelas</a>
+      <a href="{{ route('kontak') }}"
+        class="{{ request()->routeIs('kontak') ? 'text-blue-500 font-bold' : '' }}">Contact</a>
+    </div>
+  </nav>
+</div>
