d142637e216efee97b13ac0368e714b88df8c46d
Nur Aria Hibnastiar <96623047+hibnastiar27@users.noreply.github.com>
2025-01-08 21:37:20 +0700

    add: [breeze] dashboard route and profile management with authentication

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,16 @@ Route::get('/', function () {
 });
 
 
-Route::get('/about', function () {
-  return view('about', [
-    'nama' => 'Aria',
-  ]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/blog', [BlogController::class, 'index'])->name('blog');
-Route::get('/blog/{slug}', [BlogController::class, 'show'])->name('blog.show');
-Route::get('/kontak', [KontakController::class, 'index'])->name('kontak');
-Route::get('/kelas', [KelasController::class,'index'])->name('kelas');
-Route::get('/kelas/{slug}', [KelasController::class, 'show'])->name('kelas.show');
+
+
+require __DIR__.'/auth.php';
